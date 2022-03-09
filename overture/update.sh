fac976aa96d53080daf3a562d91d41775f60f481
dnomd343 <i@343.re>
2022-03-09 15:30:38 +0800

    update: overture config

--- overture/update.sh
+++ overture/update.sh
@@ -1,14 +1,15 @@
-LIST_DIR="/etc/cleardns/list"
-TEMP_DIR="$LIST_DIR/temp"
-[ -f "$LIST_DIR/no_auto_update" ] && exit
-rm -rf $TEMP_DIR
-mkdir -p $TEMP_DIR
-wget -P $TEMP_DIR https://res.343.re/Share/chinalist/chinalist.txt
-wget -P $TEMP_DIR https://res.343.re/Share/gfwlist/gfwlist.txt
-wget -P $TEMP_DIR https://raw.fastgit.org/17mon/china_ip_list/master/china_ip_list.txt
-[ -s "$TEMP_DIR/chinalist.txt" ] && mv -f $TEMP_DIR/chinalist.txt $LIST_DIR/
-[ -s "$TEMP_DIR/gfwlist.txt" ] && mv -f $TEMP_DIR/gfwlist.txt $LIST_DIR/
-[ -s "$TEMP_DIR/china_ip_list.txt" ] && mv -f $TEMP_DIR/china_ip_list.txt $LIST_DIR/
-rm -rf $TEMP_DIR
-ps -ef | grep overture | grep -v grep | grep -v sh | awk '{print $1}' | xargs kill -9
-/usr/bin/overture -c /etc/overture/config.yml > /dev/null 2>&1 &
+ASSET_DIR="/etc/cleardns/asset"
+[ -f "$ASSET_DIR/no_auto_update" ] && exit
+
+TEMP_DIR="$ASSET_DIR/temp"
+rm -rf "$TEMP_DIR" && mkdir -p "$TEMP_DIR"
+wget -P "$TEMP_DIR" https://res.dnomd343.top/Share/gfwlist/gfwlist.txt
+wget -P "$TEMP_DIR" https://res.dnomd343.top/Share/chinalist/chinalist.txt
+wget -P "$TEMP_DIR" https://res.dnomd343.top/Share/chinalist/china_ip.txt
+
+[ -s "$TEMP_DIR/gfwlist.txt" ] && mv -f "$TEMP_DIR/gfwlist.txt" "$ASSET_DIR"
+[ -s "$TEMP_DIR/chinalist.txt" ] && mv -f "$TEMP_DIR/chinalist.txt" "$ASSET_DIR"
+[ -s "$TEMP_DIR/china_ip.txt" ] && mv -f "$TEMP_DIR/china_ip.txt" "$ASSET_DIR"
+
+rm -rf "$TEMP_DIR"
+pgrep overture | xargs kill
