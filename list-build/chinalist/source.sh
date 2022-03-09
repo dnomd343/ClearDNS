6b13ee4610d836717033c76d3c4e65bd2b35e082
dnomd343 <i@343.re>
2022-03-09 15:31:29 +0800

    update: remove legacy list-build

--- list-build/chinalist/source.sh
+++
@@ -1,2 +0,0 @@
-curl https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf  | sed 's/server=\///g;s/\/114.114.114.114//g'
-curl https://raw.githubusercontent.com/hq450/fancyss/master/rules/WhiteList_new.txt  | sed 's/Server=\///g;s/\///g'
