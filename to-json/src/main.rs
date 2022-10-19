e34ddf3d0ab4aa438e2988d2c902057d9ec5e723
Dnomd343 <i@343.re>
2022-10-19 20:53:13 +0800

    feat: serde json demo

--- to-json/src/main.rs
+++ to-json/src/main.rs
@@ -1,3 +1,22 @@
+use serde_json::{Value};
+
+fn json_str() -> String {
+    let json = r#"{
+  "demo": "key_1",
+  "author": "dnomd343",
+  "test": [
+    "123",
+    "234",
+    "345"
+  ]
+}"#;
+    return String::from(json);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let raw = json_str();
+    println!("JSON raw content ->\n{}", raw);
+
+    let parsed: Value = serde_json::from_str(&raw[..]).unwrap();
+    println!("Author -> {}", parsed["author"]);
 }
