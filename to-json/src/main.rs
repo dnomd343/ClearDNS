0ae72622b47f0e5fa316e68a0944cc823715dbef
Dnomd343 <i@343.re>
2022-10-31 18:06:46 +0800

    feat: add yaml decode demo

--- to-json/src/main.rs
+++ to-json/src/main.rs
@@ -1,4 +1,5 @@
-use serde_json::{Value};
+use serde_json::Value as JsonValue;
+use serde_yaml::Value as YamlValue;
 
 const JSON_STR: &str = r#"{
   "demo": "key_1",
@@ -10,11 +11,24 @@ const JSON_STR: &str = r#"{
   ]
 }"#;
 
+const YAML_STR: &str = r#"
+demo: key_1
+author: dnomd343
+test:
+  - 123
+  - 234
+  - 345
+"#;
+
 fn main() {
     println!("JSON raw content ->\n{}", JSON_STR);
+    println!("YAML raw content ->\n{}", YAML_STR);
 
-    let data: Value = serde_json::from_str(JSON_STR).unwrap();
+    // let data: JsonValue = serde_json::from_str(JSON_STR).unwrap();
+    // println!("{:#?}", data);
+    let data: YamlValue = serde_yaml::from_str(YAML_STR).unwrap();
     println!("{:#?}", data);
+
 
     let ret = serde_json::to_string(&data).unwrap();
     println!("JSON output ->\n{}", ret);
