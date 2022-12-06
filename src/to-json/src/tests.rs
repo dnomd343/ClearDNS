c13a208c2a13d1b927d622fd427d7c0e9506a2b6
Dnomd343 <i@343.re>
2022-12-06 21:47:02 +0800

    update: tests module

--- src/to-json/src/tests.rs
+++ src/to-json/src/tests.rs
@@ -1,5 +1,6 @@
 use crate::convert::to_json;
 
+#[allow(dead_code)]
 const JSON_TEST_CONTENT: &str = r#"
 {
   "int": 123,
@@ -13,6 +14,7 @@ const JSON_TEST_CONTENT: &str = r#"
 }
 "#;
 
+#[allow(dead_code)]
 const YAML_TEST_CONTENT: &str = r#"
 int: 123
 bool: true
@@ -28,6 +30,7 @@ object:
   sub: test
 "#;
 
+#[allow(dead_code)]
 const TOML_TEST_CONTENT: &str = r#"
 int = 123
 bool = true
@@ -39,33 +42,39 @@ array = [ 1, 2, 3, 4, 5 ]
 sub = "test"
 "#;
 
-fn format_json(raw: &str) -> String {
-    match to_json(raw) {
-        Some(data) => data,
-        None => panic!("JSON format error"),
+
+mod tests {
+    use super::*;
+
+    #[allow(dead_code)]
+    fn format_json(raw: &str) -> String {
+        match to_json(raw) {
+            Some(data) => data,
+            None => panic!("JSON format error"),
+        }
     }
-}
 
-#[test]
-fn json_to_json() {
-    assert_eq!(
-        format_json(JSON_TEST_CONTENT),
-        format_json(&to_json(JSON_TEST_CONTENT).unwrap()),
-    );
-}
+    #[test]
+    fn json_to_json() {
+        assert_eq!(
+            format_json(JSON_TEST_CONTENT),
+            format_json(&to_json(JSON_TEST_CONTENT).unwrap()),
+        );
+    }
 
-#[test]
-fn yaml_to_json() {
-    assert_eq!(
-        format_json(JSON_TEST_CONTENT),
-        format_json(&to_json(YAML_TEST_CONTENT).unwrap()),
-    );
-}
+    #[test]
+    fn yaml_to_json() {
+        assert_eq!(
+            format_json(JSON_TEST_CONTENT),
+            format_json(&to_json(YAML_TEST_CONTENT).unwrap()),
+        );
+    }
 
-#[test]
-fn toml_to_json() {
-    assert_eq!(
-        format_json(JSON_TEST_CONTENT),
-        format_json(&to_json(TOML_TEST_CONTENT).unwrap()),
-    );
+    #[test]
+    fn toml_to_json() {
+        assert_eq!(
+            format_json(JSON_TEST_CONTENT),
+            format_json(&to_json(TOML_TEST_CONTENT).unwrap()),
+        );
+    }
 }
